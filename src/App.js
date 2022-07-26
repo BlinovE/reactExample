42190d497595efdf8f95d42df9a46017721420de
BlinovE <95091610+BlinovE@users.noreply.github.com>
2022-07-26 18:38:52 +0300

    Component
    Вынес счётчик в отдельный компонент
    Counter.jsx .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import React, {useState} from 'react';
+import React from 'react';
+import Counter from './components/Counter.jsx';
 
 function App() {
-  const [count, setCount] = useState(5);
-  const [value, setValue] = useState('Привет инпут');
-  
-  function inc(){
-    setCount(count+1);
-  }
-  function dec(){
-    setCount(count-1);
-  }
   
   return (
     <div className="App">
-      <h1>{count}</h1>
-      <h1>{value}</h1>
-      <input type="text" value={value} onChange={(event)=>{
-        setValue(event.target.value)
-      }}/>
-      <button onClick={ inc }>Increment</button>
-      <button onClick={ dec }>Decrement</button>
+      
+      <Counter/>
+      
     </div>
   );
 }
