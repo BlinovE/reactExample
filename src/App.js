4d23759effb65c07da644cd9d458ee2e652050f5
BlinovE <95091610+BlinovE@users.noreply.github.com>
2022-07-26 18:57:09 +0300

    Add Component Post
    Добавил компонент Post

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Counter from './components/Counter.jsx';
+import Post from './components/Post.jsx';
+import './styles/App.css';
 
 function App() {
   
@@ -7,6 +9,7 @@ function App() {
     <div className="App">
       
       <Counter/>
+      <Post/>
       
     </div>
   );
