d79564a11f2ac93729bd69c6437c70bb05bc1402
BlinovE <95091610+BlinovE@users.noreply.github.com>
2022-07-26 22:28:06 +0300

    UI MyInput
    Создал UI компонент MyInput

---
+++ src/components/UI/input/MyInput.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import inp_class from './MyInput.module.css';
+
+const MyInput = function({children,...props}){
+    return(
+        <input {...props} className={inp_class.myInp}/>
+    );
+}
+
+export default MyInput;
