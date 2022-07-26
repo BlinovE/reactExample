da3f660ac8f4916d86317f1fae47b9321904f332
BlinovE <95091610+BlinovE@users.noreply.github.com>
2022-07-26 19:04:34 +0300

    Добавление props
    Использование props с компонентом Post

--- src/App.js
+++ src/App.js
@@ -4,15 +4,48 @@ import Post from './components/Post.jsx';
 import './styles/App.css';
 
 function App() {
-  
-  return (
-    <div className="App">
-      
-      <Counter/>
-      <Post/>
-      
-    </div>
-  );
+
+    return (
+        <div className="App">
+
+            <Counter/>
+            <Post post={
+                {
+                    id: 1,
+                    title: 'JavaScript',
+                    body: 'Description about JS.'
+                }
+            }/>
+            <Post post={
+                {
+                    id: 2,
+                    title: 'Python',
+                    body: 'Description about Py.'
+                }
+            }/>
+            <Post post={
+                {
+                    id: 3,
+                    title: 'Go',
+                    body: 'Description about Go.'
+                }
+            }/>
+            <Post post={
+                {
+                    id: 4,
+                    title: 'Dart',
+                    body: 'Description about Dart.'
+                }
+            }/>
+            <Post post={
+                {
+                    id: 6,
+                    title: 'Rust',
+                    body: 'Description about Rust.'
+                }
+            }/>
+        </div>
+    );
 }
 
 export default App;
