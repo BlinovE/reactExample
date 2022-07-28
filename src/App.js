37eb2c0661c9ad709eeb17cc1b2f340c3dd943d5
BlinovE <95091610+BlinovE@users.noreply.github.com>
2022-07-28 23:25:51 +0300

    Сортировка
    Добавлен UI компонент сортировки по названию и по описанию

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import PostsList from './components/PostsList';
 import PostForm from './components/PostForm';
 import './styles/App.css';
+import MySelect from './components/UI/select/select';
 
 function App() {
     const [posts, setPosts] = useState([
@@ -20,25 +21,37 @@ function App() {
         }
     ])
     // функция обратного вызова для создания нового объекта массива posts
-    const createPost = (newPost)=>{
-      setPosts([...posts,newPost]);
+    const createPost = (newPost) => {
+        setPosts([
+            ...posts,
+            newPost
+        ]);
     }
 
-    const removePost = (post)=>{
-      setPosts(posts.filter(p=>p.id !== post.id));
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id));
     }
-    
+
+    const sortPosts= (sort)=>{
+      setSelectedSort(sort);
+      setPosts([...posts].sort((a,b)=> a[sort].localeCompare(b[sort])))
+    }
+
+    const [selectedSort, setSelectedSort] = useState('');
+
     return (
         <div className="App">
-            <PostForm create={createPost} />
-            {
-              posts.length?
-              <PostsList remove={removePost} posts={posts}
-                title="Список постов 1"/>:
-                <h1>Посты не найдены</h1>
-            }
-            
-        </div>
+            <PostForm create={createPost}/>
+            <hr style={{margin:'15px 0'}}/>
+            <MySelect value={selectedSort} onChange={sortPosts} defaultValue="Сортировка" options={[
+              {value:"title", name:"По названию"},
+              {value:"body", name:"По опиcанию"}
+            ]}/>
+             {
+            posts.length ? <PostsList remove={removePost}
+                posts={posts}
+                title="Список постов 1"/> : <h1>Посты не найдены</h1>
+        } </div>
     );
 }
 
